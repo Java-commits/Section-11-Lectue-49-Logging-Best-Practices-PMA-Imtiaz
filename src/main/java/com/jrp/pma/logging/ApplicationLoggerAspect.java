9b815cf31f392865793cc7a4324e90284b00980b
Java-Commits <narensedam.github@gmail.com>
2020-06-07 15:08:57 +0530

    Logging best practices

---
+++ src/main/java/com/jrp/pma/logging/ApplicationLoggerAspect.java
@@ -0,0 +1,28 @@
+package com.jrp.pma.logging;
+
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class ApplicationLoggerAspect {
+	
+	
+	private final Logger log = LoggerFactory.getLogger(this.getClass());
+	
+	@Pointcut("within(com.jrp.pma.controllers..*)")
+	public void definePackagePoints() {
+		
+	}
+	
+	@After("definePackagePoints()")
+//	@Before("definePackagePoints()")
+	public void log() {
+		log.debug("--------------------------------");
+	}
+
+}
